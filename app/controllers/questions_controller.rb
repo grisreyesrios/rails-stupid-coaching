43cc109ef2d48732ffb99b7b5b852b2b570873b0
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2018-11-12 19:39:29 +0100

    Implement testing to rails-stupid-coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,9 +8,9 @@ class QuestionsController < ApplicationController
     if @question == 'I am going to work'
       @answer = 'Great'
     elsif @question.include? '?'
-      @answer = 'Silly question, get dressed and go to work!'
+      @answer = "Silly question, get dressed and go to work!"
     else
-      @answer = 'I do not care, get dressed and go to work!'
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
